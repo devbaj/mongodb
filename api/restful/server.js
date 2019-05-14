247858b3f2b51f385d188e10dd2da350ceaf73d1
Brett Jory <bjory7@gmail.com>
2019-05-14 15:55:08 -0700

    completed assignment hello angular with bonus challenge

--- api/restful/server.js
+++ api/restful/server.js
@@ -2,6 +2,7 @@ const express = require ( 'express' );
 const app = express ( );
 const bodyParser = require ( 'body-parser' );
 app.use ( bodyParser.json ( ) );
+app.use ( express.static ( __dirname + '/public/dist/public' ) );
 require ( './routes' ) ( app );
 const server = app.listen ( 8000 , ( ) => {
 	console.log ( 'server listening on port 8000' );
