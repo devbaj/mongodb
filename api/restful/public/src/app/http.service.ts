d92581b380fd56482f2cc3c4e28fdffd3fdff81d
Brett Jory <bjory7@gmail.com>
2019-05-15 10:12:27 -0700

    i dunno

---
+++ api/restful/public/src/app/http.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http' ;
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor( private _http : HttpClient ) {
+    this.getTasks() ;
+  }
+
+  getTasks( ) {
+    let tempObservable = this._http.get( '/tasks' ) ;
+    tempObservable.subscribe( data => console.log( 'Got tasks' , data ) ) ;
+  }
+
+  getOneTask( ) {
+    let tempObservable = this._http.get( '/tasks/' ) // ! what da fuq???
+  }
+
+}
